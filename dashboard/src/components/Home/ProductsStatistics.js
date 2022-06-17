9988e781b8991e6613045e8e30aee6892adc4100
Malhomme <farrellndv770@gmail.com>
2022-06-17 14:53:36 +0200

    shopNext

---
+++ dashboard/src/components/Home/ProductsStatistics.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const ProductsStatistics = () => {
+  return (
+    <div className="col-xl-6 col-lg-12">
+      <div className="card mb-4 shadow-sm">
+        <article className="card-body">
+          <h5 className="card-title">Products statistics</h5>
+          <iframe
+            style={{
+              background: "#FFFFFF",
+              border: "none",
+              borderRadius: "2px",
+              boxShadow: "0 2px 10px 0 rgba(70, 76, 79, .2);",
+              width: "100%",
+              height: "350px",
+            }}
+            src="https://charts.mongodb.com/charts-fullshopnext-dxgna/embed/charts?id=62a4f7f7-cc45-4587-85d1-83d52acab291&maxDataAge=3600&theme=light&autoRefresh=true"
+          ></iframe>
+        </article>
+      </div>
+    </div>
+  );
+};
+
+export default ProductsStatistics;
---
+++ dashboard/src/components/Home/SalesStatistics.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const SaleStatistics = () => {
+  return (
+    <div className="col-xl-6 col-lg-12">
+      <div className="card mb-4 shadow-sm">
+        <article className="card-body">
+          <h5 className="card-title">Sale statistics</h5>
+          <iframe
+            style={{
+              background: "#FFFFFF",
+              border: "none",
+              borderRadius: "2px",
+              boxShadow: "0 2px 10px 0 rgba(70, 76, 79, .2);",
+              width: "100%",
+              height: "350px",
+            }}
+            src="https://charts.mongodb.com/charts-fullshopnext-dxgna/embed/charts?id=62a4f5b6-8f99-4114-8877-195c350d5f1c&maxDataAge=3600&theme=light&autoRefresh=true"
+          ></iframe>
+        </article>
+      </div>
+    </div>
+  );
+};
+
+export default SaleStatistics;
